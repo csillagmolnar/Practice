9d383498737b29a139764a39ff6ef9b0331f3b55
csillagmolnar <csillag.molnar01@gmail.com>
2023-09-29 16:47:31 +0200

    Add lookup_password()

--- authentication/leaked_password.py
+++ authentication/leaked_password.py
@@ -8,3 +8,19 @@ except ImportError:
     print('Install requests package')
     exit()
 
+def lookup_password(password):
+    pwd_hash = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    pwd_hash_head = pwd_hash[:5]
+    pwd_hash_tail = pwd_hash[5:]
+    URL = 'https://api.pwnedpasswords.com/range/' + pwd_hash_head
+    
+    try:
+        res = requests.get(URL)
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(e)
+
+    res_result = (line.split(':') for line in res.text.splitlines())
+
+    count = next(count for hash_tail, count in res_result if hash_tail == pwd_hash_tail)
+
+    return pwd_hash, count
